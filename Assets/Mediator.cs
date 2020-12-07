5d647ce3a44b6e2c478fb2fa78b7228f2cc349e8
sahar <sahar.malter1@gmail.com>
2020-12-07 11:36:25 +0200

    fix the mediator

--- Assets/Mediator.cs
+++ Assets/Mediator.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Mediator : MonoBehaviour
+public class mediator : MonoBehaviour
 {
-
+    public static int direction;
+    public GameObject plane;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = 1;
     }
 
     // Update is called once per frame
@@ -17,21 +19,18 @@ public class Mediator : MonoBehaviour
         
     }
 
-    public void get_press(GameObject child)
+    public void get_plane_info()
     {
-
-        StartCoroutine(change_color(child));
+        StartCoroutine(check_direct());
     }
-    public IEnumerator change_color(GameObject child)
-    {
-        Material mat;
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            mat = transform.GetChild(i).GetComponent<MeshRenderer>().material;
-            transform.GetChild(i).GetComponent<MeshRenderer>().material = child.GetComponent<MeshRenderer>().material;
-            yield return new WaitForSeconds(0.5f);
-            transform.GetChild(i).GetComponent<MeshRenderer>().material = mat;
-        }
 
+    public IEnumerator check_direct() // i know i know, but you got the point
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("this is TOWER let me check the direction");
+        direction *= -1;
+        yield return new WaitForSeconds(3f);
+        Debug.Log("omg this is the wrong direction, change to this direction");
+        plane.GetComponent<plane>().get_tower_info();
     }
 }
---
+++ Assets/plane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class plane : MonoBehaviour
+{
+    public int direction;
+    public GameObject tower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(get_direct());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,1,0) * direction);
+        
+    }
+
+    public void get_tower_info()
+    {
+        StartCoroutine(get_direct());
+    }
+
+    public IEnumerator get_direct()
+    {
+        yield return new WaitForSeconds(2f);
+        direction = mediator.direction;
+        Debug.Log("this is PLANE i got new direction, and i fly accordingly");
+        yield return new WaitForSeconds(3f);
+        Debug.Log("wait... is my direction ok?");
+        tower.GetComponent<mediator>().get_plane_info();
+    }
+}
